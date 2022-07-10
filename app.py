210ab2a05bc555f0bc817afbc9b98fbe216a4c3f
Roger Bosch <boschr@elearningmedia.es>
2022-07-10 16:11:48 +0200

    Update to selenium 4.1

--- app.py
+++ app.py
@@ -9,21 +9,21 @@ class TwitterBot:
         self.username = username
         self.password = password
         self.bot = webdriver.Chrome(
-            executable_path='D:\\Oshytech\\Codigo\\Python\\TwitterBot\\chromedriver.exe')
+            executable_path='/home/rbosch/Descargas/chromedriver')
 
     def login(self):
         bot = self.bot
         bot.get('https://twitter.com/login')
         # En la nueva versión del login de twitter, el login está segmentado en dos partes
         time.sleep(3)
-        email = bot.find_element_by_xpath('//input[@name="username"]')
+        email = bot.find_element("xpath", '//input[@autocomplete="username"]')
         # limpiamos por si las moscas
         email.clear()
         email.send_keys(self.username)
         email.send_keys(Keys.RETURN)
         time.sleep(3)
 
-        password = bot.find_element_by_xpath('//input[@name="password"]')
+        password = bot.find_element("xpath", '//input[@name="password"]')
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
         time.sleep(3)
@@ -37,14 +37,14 @@ class TwitterBot:
             bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
             time.sleep(2)
 
-        tweets = bot.find_elements_by_xpath('//a[@dir="auto" and not(@rel)]')
+        tweets = bot.find_element("xpath", '//a[@dir="auto" and not(@rel)]')
         tweets_url = [tweet.get_attribute('href') for tweet in tweets]
 
         for url in tweets_url:
             bot.get(url)
             try:
                 time.sleep(3)
-                likeButton = bot.find_element_by_xpath('//div[@data-testid="like"]')
+                likeButton = bot.find_element("xpath", '//div[@data-testid="like"]')
                 likeButton.click()
                 time.sleep(15)
             except Exception as ex:
